423ac87df2e6e7c1deecfbe5808cfd6378902e5d
solvecaptcha-com <info@solvecaptcha.com>
2025-03-26 16:28:20 +0200

    Update __init__.py

--- solvecaptcha/__init__.py
+++ solvecaptcha/__init__.py
@@ -1,3 +1,15 @@
 from .api import ApiClient
 from .solver import (Solvecaptcha, SolverExceptions, ValidationException,
                      NetworkException, ApiException, TimeoutException)
+
+"""
+Python 3 package for easy integration with the API of solvecaptcha.com captcha solving service
+to bypass ReCaptcha, Funcaptcha, Geetest and solve any other captchas.
+
+Website: https://solvecaptcha.com/
+Support: indo@solvecaptcha.com
+License: MIT
+"""
+
+__author__ = 'SolveCaptcha'
+__version__ = '1.0.0'
